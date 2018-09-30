86fef4f61898f86ef55743a84430317eb7d5e1fd
AnzhelaHulnik <anzgul@tut.by>
2018-09-30 11:13:56 +0300

    Added php select

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <h1>HELLO</h1>
+<h2>Beautiful world</h2>
+
+
+<?php
+$year = 2000;
+?>
+<select>
+
+        <?php
+        for($year; $year <= 2050; $year++){
+            echo"<option>$year</option>";
+           }
+        ?>
+
+</select>
+
+
